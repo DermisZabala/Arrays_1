7aecd43a0a5de3e9036a976ca9b65fd9cf2a6b5c
DermisZabala <dermiszabala@gmail.com>
2024-05-16 18:22:15 -0400

    Declaracion e inicializacion a una Matriz

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = new string[5] {"Pedro", "Juan", "Maria", "Teresa", "Hamel"};
+            int[] edades = new int[5];
+            edades[0] = 19;
+            edades[1] = 30;
+            edades[2] = 24;
+            edades[3] = 50;
+            edades[4] = 91;
+
+            Console.WriteLine($"Nombres \tEdades\n{nombres[0]} \t\t{edades[0]}\n{nombres[1]} \t\t{edades[1]}\n" +
+                $"{nombres[2]} \t\t{edades[2]}\n{nombres[3]} \t\t{edades[3]}\n" +
+                $"{nombres[4]} \t\t{edades[4]}\n");
+
+            Console.ReadKey();
+        }
+    }
+}
